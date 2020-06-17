8d989f7753f6cd10c804b4ffa7847dbd2cf2e88e
servibilis <servibilis@gmail.com>
2020-06-17 16:22:57 +0300

    17

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fscss.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+			  presets: ['@babel/preset-env']
+			}
+		  }
+      }
+    ]
+  }
+
+};
+
+module.exports = config;
